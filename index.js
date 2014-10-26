1de73f3463ebe6939fe87d7dbcb370eaddc25b8f
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-10-27 03:15:52 +0800

    change logger, and auto release 0.0.8

--- index.js
+++ index.js
@@ -5,44 +5,43 @@ var _ = require('lodash'),
  * 该模块负责自动将 bus.data('respond') 中的数据输出到浏览器端。
  * @module respond
  */
-module.exports = {
-  deps: ['request','model', 'bus'],
+var respondModule = {
   route : {
     "*" : {
       "function" :function respondHandler( req, res, next){
 //        if( req.isAgent && !req.isFirstAgent) return next && next()
-
-        ZERO.mlog("respond","<====== respond default handler take action","isAgent:",req.isAgent)
+        var logger = respondModule.dep.logger
+        logger.log("respond","<====== respond default handler take action","isAgent:",req.isAgent)
 
         //must wait all result resolved!
         req.bus.then(function(){
-          ZERO.mlog("respond","respond bus.then execute ")
+          logger.log("respond","respond bus.then execute ")
             var respond = req.bus.data('respond')
 
             if( !respond ){
-              ZERO.mlog("respond"," NOTHING HAPPENED", req.bus._id )
+              logger.log("respond"," NOTHING HAPPENED", req.bus._id )
               res.status(404).send("404")
             }else{
 
               if( respond.file ){
                 return req.bus.fire('respond.file.before', respond).then(function(){
-                  ZERO.mlog("respond","file ======>".cyan,respond.file)
+                  logger.log("respond","file ======>".cyan,respond.file)
                   res.sendFile( respond.file)
                 })
               }else if( respond.page ){
                 return req.bus.fire('respond.page.before', respond).then(function() {
-                  ZERO.mlog("respond","page ======>".blue,respond.page, JSON.stringify(Object.keys(respond.data)))
+                  logger.log("respond","page ======>".blue,respond.page, JSON.stringify(Object.keys(respond.data)))
                   res.render(respond.page, respond.data || {})
                 })
               }else{
                 return req.bus.fire('respond.data.before', respond).then(function() {
-                  ZERO.mlog("respond","data ======>".green,Object.keys(respond.data).join(","))
+                  logger.log("respond","data ======>".green,Object.keys(respond.data).join(","))
                   res.json(respond.data || {})
                 })
               }
             }
         }).catch(function( err ){
-          ZERO.error("respond last handler error",err)
+          logger.error("respond last handler error",err)
           res.status(err.status || 500).json({errors: req.bus.$$error })
         })
 
@@ -53,3 +52,5 @@ module.exports = {
     }
   }
 }
+
+module.exports = respondModule
